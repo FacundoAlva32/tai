36339950d0b7c2fa4ee77b5cf28020908b73cab2
FacundoAlva32 <facundoalva32@gmail.com>
2026-02-11 17:40:23 -0300

    deploy render no hacer

--- shared_space/settings.py
+++ shared_space/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c+u86r8zh2rltvie+4kaeshx^m)rw&+47dnq=$%+u*c(=3n^6r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
